becdba448fd6d50d4ece6cf8de6220bd144463de
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-01-24 07:49:15 +0000

    fix: deployment router paths

--- api/handlers/deployments/router.go
+++ api/handlers/deployments/router.go
@@ -9,8 +9,8 @@ import (
 func Router(s app.ApplicationServices, r chi.Router) {
 	r.Route("/project/{projectId}/deployment", func(r chi.Router) {
 
-		r.Post("/{projectId}/deployment", CreateDeployment(s))
-		r.Get("/{projectId}/deployment/{deploymentId}", FindDeploymentById(s))
-		r.Delete("/{projectId}/deployment/{deploymentId}", BuangDeployment(s))
+		r.Post("/", CreateDeployment(s))
+		r.Get("/{deploymentId}", FindDeploymentById(s))
+		r.Delete("/{deploymentId}", BuangDeployment(s))
 	})
 }
