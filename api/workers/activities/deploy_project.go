ea44f57544c9ce46f51aee75e7d07fb78ae4db86
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-02-13 21:17:39 +1300

    fix: expansion envs (#40)

--- api/workers/activities/deploy_project.go
+++ api/workers/activities/deploy_project.go
@@ -102,15 +102,19 @@ func (dp *DeployProject) DeployProject(ctx context.Context, d DeployProjectParam
 	})
 	url := fmt.Sprintf("/deployment/%v", projectName)
 
-	env["BUANG_DEPLOYMENT_PATH"] = url
-
 	traefikEntrypointRouter := projectName
 	traefikEntrypointService := projectName
+
+	env["BUANG_DEPLOYMENT_PATH"] = url
+	expansionEnvs := map[string]string{}
+	for k, v := range env {
+		expansionEnvs[k] = fmt.Sprintf("%v", v)
+	}
+	expansionEnvs["BUANG_ENTRYPOINT_TRAEFIK_ROUTER"] = traefikEntrypointRouter
+	expansionEnvs["BUANG_ENTRYPOINT_TRAEFIK_SERVICE"] = traefikEntrypointService
+
 	composePath := filepath.Join(d.Dir, p.Project.GetComposePath())
-	expandedComposePath, _, err := docker.ExpandComposeYaml(composePath, map[string]string{
-		"BUANG_ENTRYPOINT_TRAEFIK_ROUTER":  traefikEntrypointRouter,
-		"BUANG_ENTRYPOINT_TRAEFIK_SERVICE": traefikEntrypointService,
-	})
+	expandedComposePath, _, err := docker.ExpandComposeYaml(composePath, expansionEnvs)
 	if err != nil {
 		return nil, err
 	}
